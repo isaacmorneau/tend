14d27f27181cd6b227d74d344a4992b0574310a5
Isaac Morneau <isaacmorneau@gmail.com>
2019-03-04 00:55:54 -0800

    added ability to track relations between entities

--- tables.sql
+++ tables.sql
@@ -48,3 +48,28 @@ before update on alias
 for each row
 execute procedure trigger_update_timestamp();
 
+create table relation (
+    id serial not null primary key,
+    entity_id_a integer not null references entity(id),
+    entity_id_b integer not null references entity(id),
+    created timestamp not null default now(),
+    updated timestamp not null default now()
+);
+
+create trigger set_timestamp
+before update on relation
+for each row
+execute procedure trigger_update_timestamp();
+
+create table relation_snippet (
+    id serial not null primary key,
+    relation_id integer not null references relation(id),
+    data text not null,
+    created timestamp not null default now(),
+    updated timestamp not null default now()
+);
+
+create trigger set_timestamp
+before update on relation_snippet
+for each row
+execute procedure trigger_update_timestamp();
